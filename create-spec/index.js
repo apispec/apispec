03cb7c41200542edef5f3b7c0decf9c13aa49273
azahnen <zahnen@interactive-instruments.de>
2019-09-02 16:10:38 +0200

    init monorepo

---
+++ create-spec/index.js
@@ -0,0 +1,40 @@
+#!/usr/bin/env node
+
+const commander = require('commander');
+const chalk = require('chalk');
+
+const createSpec = require('./create-spec');
+const packageJson = require('./package.json');
+
+const program = new commander.Command()
+  .name(packageJson.name)
+  .usage(`${chalk.green('<project-directory>')} [options]`)
+  .arguments('<project-directory>')
+  .action(name => {
+    projectName = name;
+  })
+  .option('-j, --json', 'add json asserts')
+//  .allowUnknownOption()
+  .on('--help', () => {
+    console.log();
+    console.log(`    Only ${chalk.green('<project-directory>')} is required.`);
+    console.log();
+  })
+  .version(packageJson.version, '-v, --version', 'output the current version')
+  .parse(process.argv);
+
+//if (!process.argv.slice(2).length) {
+//  program.outputHelp();
+//}
+
+if (typeof projectName === 'undefined') {
+  program.outputHelp();
+  process.exit(0);
+}
+
+createSpec(
+  projectName,
+  program.verbose,
+  program.json
+);
+
